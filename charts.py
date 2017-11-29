3991438b526a6502811458030da1011f6e6676db
Louise Corrigan <D16124823@mydit.ie>
2017-11-29 20:54:20 +0000

    charts, ages, quotes

--- charts.py
+++ charts.py
@@ -7,15 +7,30 @@ browser."""
 from flask import Flask, render_template
 app = Flask(__name__)
 
+# ages = [('John',10),('Pat',12),('Sophie',14)]
+# John is 10
+# Pat is 12
+# Sophie is 14
+
+@app.route("/age")
+def age ():
+	x = [('john', 10,"address"), ('pat', 12), ('sophie', 14)]
+	return render_template('age.html', boysAges=x)
+
+
+@app.route("/quotes/<string:name>")
+def quote(name):
+    return render_template('quote.html', name=name)
+
 @app.route('/chart/')
-def hello():
+def chart():
 	# The data can come from anywhere you can read it; for instance, a SQL
 	# query or a file on the filesystem created by another script.
 	# This example expects two values per row; for more complicated examples,
 	# refer to the Google Charts gallery.
 	data = [('Sunday', 48), ('Monday', 27), ('Tuesday', 32), ('Wednesday', 42),
 			('Thursday', 38), ('Friday', 45), ('Saturday', 52)]
-	return render_template('template.html', data=data)
+	return render_template('chart.html', data=data)
 
 if __name__ == '__main__':
 	# Automatically detect changes to charts.py and reload the server as
